6e1ec0baf37a203715296b869a4e6338e7379a76
mrMarcoGiovanne <marco.giovanne.jalotjot@gmail.com>
2019-10-08 09:30:35 -0700

    create java folder

---
+++ src/main/java/MyntApplication.java
@@ -0,0 +1,17 @@
+package com.mynt.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 
+ * @author Marco Giovanne
+ *
+ */
+@SpringBootApplication
+public class MyntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyntApplication.class, args);
+	}
+}
